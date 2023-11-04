15fbe9e4f2a787a16937cc46293ee42e04b8abf2
Facundo Villalba <vfacu44@gmail.com>
2023-11-03 21:29:05 -0300

    moificando formulario de registro

--- PetPulse/app/src/main/java/com/proyectdevispc/petpulse/Form_registrarse.java
+++ PetPulse/app/src/main/java/com/proyectdevispc/petpulse/Form_registrarse.java
@@ -1,8 +1,11 @@
 package com.proyectdevispc.petpulse;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Form_registrarse extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class Form_registrarse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_registrarse);
+    }
+    public void btn_cancelar(View view){
+        Intent form_login= new Intent(this, MainActivity.class);
+        startActivity(form_login);
     }
 }
