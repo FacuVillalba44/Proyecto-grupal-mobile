f314fb60a437c96f80cde331a04c69729b1d6228
20fer20 <fjuare34@gmail.com>
2023-11-08 08:00:27 -0300

    creando la base de datos de veterinarias

--- app/src/main/java/com/fj/developferjuarez/activityVeterinaria.java
+++ app/src/main/java/com/fj/developferjuarez/activityVeterinaria.java
@@ -3,10 +3,25 @@ package com.fj.developferjuarez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android content.context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class activityVeterinaria extends AppCompatActivity {
-Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_veterinaria);
+public class UsuariosSQLiteHelper extends SQLiteOpenHelper {
+    String sqlCreate = "CREATE TABLE Usuarios (codigo INTEGER, nombre TEXT, direccion TEXT");
+    public UsuariosSQLiteHelper (Context contexto, String nombre, CursorFactory factory, int version) {
+        super(contexto, nombre, direccion, factory, version)
     }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        db.execSQL(sqlCreate);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int versionAnterior, int versionNueva) {
+        db.execSQL("DROP TABLE IF EXISTS Usuarios");
+        db.execSQL(sqlCreate);
+    }
+}
