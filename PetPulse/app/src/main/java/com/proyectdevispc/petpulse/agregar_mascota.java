2cbc80b96693263e2a20ac8789500a3e8030a63a
Facundo Villalba <vfacu44@gmail.com>
2023-11-09 01:50:30 -0300

    Aplicando estilos a menu principal, añadiendo pantallas, agregando personalización de icono

---
+++ PetPulse/app/src/main/java/com/proyectdevispc/petpulse/agregar_mascota.java
@@ -0,0 +1,14 @@
+package com.proyectdevispc.petpulse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class agregar_mascota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_mascota);
+    }
+}
