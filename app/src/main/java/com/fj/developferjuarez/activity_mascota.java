5fcac50a31e123788e36b8eb93f6d3a6ba17a809
20fer20 <fjuare34@gmail.com>
2023-11-13 19:51:24 -0300

    codigo botones

--- app/src/main/java/com/fj/developferjuarez/activity_mascota.java
+++ app/src/main/java/com/fj/developferjuarez/activity_mascota.java
@@ -2,13 +2,24 @@ package com.fj.developferjuarez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_mascota extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mascota);
+        Button btnVolverveter = findViewById(R.id:btnVolverveter);
+
+        btnVolverveter.setOnClickListener(new View.OnClickListener() {
+            Intent intent = new Intent(activity_mascota.this, activity_mascota.class );
+            startActivity(intent);
+        });
+
     }
 }
