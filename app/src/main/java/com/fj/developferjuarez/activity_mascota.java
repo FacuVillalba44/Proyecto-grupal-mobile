89b8142fe7afaf49a9e77d6a302aa19c5d7ab67f
20fer20 <fjuare34@gmail.com>
2023-11-14 19:58:17 -0300

    database mascotas

--- app/src/main/java/com/fj/developferjuarez/activity_mascota.java
+++ app/src/main/java/com/fj/developferjuarez/activity_mascota.java
@@ -3,22 +3,77 @@ package com.fj.developferjuarez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class activity_mascota extends AppCompatActivity {
-
-
+ private MascotaDataSource dataSource;
+ private EditText editTextNombre, getEditTextRaza;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mascota);
         Button btnVolverveter = findViewById(R.id:btnVolverveter);
+        dataSource = new MascotaDataSource(this);
+        dataSource.open();
+        editTextNombre = findViewById(R.id.editTextNombre);
+        getEditTextRaza = findViewById(R.id.editTextRaza);
+        button btnGuardar = findViewById(R.id.btnGuardar);
+
+        btnGuardar.setOnClickListener(new view.OnClickListener()){
+            public void onclick(View v) {
+                guardarMascota();
+            }
+        });
+        obtenerYMostrarMascotas();
+    }
+
+    private void guardarMascota() {
+        String nombre   = editTextNombre.getText().toString();
+        String raza = editTextRaza.getText().toString();
+
+        if(!nombre.isEmpty() && !raza.isEmpty()) {
+            dataSource.insertMascota(nombre, raza);
+
+            editTextNombre.getText().clear();
+            getEditTextRaza.getText().clear();
+
+            obtenerYMostrarMascotas();
+        } else {
+            Object toast;
+            toast.makeText(this, "Por favor, ingrese nombre y raza", toast.length_short).show();
+
+        }
+    }
+        private void obtenerYMostrarMascotas() {
+            Cursor cursor = dataSource.getAllMascotas();
+            if (cursor.moveToFirst()) {
+                do{
+
+                    String nombre = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_NOMBRE));
+                    String raza = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_RAZA));
+
+                    toast.makeText(this, "Nombre: "+ nombre + ", Raza: " + raza, Toast.LENGTH_SHORT).show();
+
+                }while (cursor.moveToNext());
+
+                }
+            }
+
+            protected void onDestroy() {
+        super.onDestroy();
+        dataSource.close();
+            }
+        }
 
         btnVolverveter.setOnClickListener(new View.OnClickListener() {
             Intent intent = new Intent(activity_mascota.this, activity_mascota.class );
             startActivity(intent);
+
         });
 
     }
