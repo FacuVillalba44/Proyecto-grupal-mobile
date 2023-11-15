e8f76cdb9bfc06fe289ae58197838d4f17e47223
20fer20 <fjuare34@gmail.com>
2023-11-14 21:45:10 -0300

    actualizando mascotas y base de datos

---
+++ app/src/main/java/com/fj/developferjuarez/activity_listado_veterinarias.java
@@ -0,0 +1,14 @@
+package com.fj.developferjuarez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_listado_veterinarias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_veterinarias);
+    }
+}
